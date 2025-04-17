fa8d1de04c3be42f330fe0892656c4baa6ed78ee
Oppie1 <atindall729@gmail.com>
2025-04-17 18:00:26 -0500

    Add files via upload

---
+++ 1n2CPrntTxtOnScrnNComnts/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*this is my program it prints out his name 
+it 
+is 
+awesome
+Do this because it prints on multiple lines
+*/
+
+
+
+int main()
+{
+	//These are the lines that print his name on one line
+
+	printf("Bucky is awesome\a");  //the \a makes a noise. cool!
+	printf("Bucky is cool");
+
+		return 0;
+}
